b4a54fb9551e30ae8ac553f1e8bc0d4335319a96
Sobhan m <veiledassassin99@gmail.com>
2023-04-15 19:16:51 +0430

    find-near-restaurant test

--- src/modules/Customer/__tests__/usecases/findNearRestaurant/find-near-restaurant.spec.ts
+++ src/modules/Customer/__tests__/usecases/findNearRestaurant/find-near-restaurant.spec.ts
@@ -1,13 +1,24 @@
 import { FindNearRestaurant } from "../../../usecases/findNearRestaurant/FindNearRestaurant"
+import { FindNearRestaurantDTO } from "../../../usecases/findNearRestaurant/FindNearRestaurantDTO"
 
 
 describe('Find near restaurant useCase', () => {
     let useCase: FindNearRestaurant
 
-    beforeEach(()=> {
+    beforeEach(() => {
         useCase = new FindNearRestaurant()
     })
+
     it('should exists', () => {
-       expect(useCase).toBeDefined()
+        expect(useCase).toBeDefined()
+    })
+
+    it('should return list of restaurants', async () => {
+        const dto: FindNearRestaurantDTO = {
+            city: 'rasht'
+        }
+
+        const result = await useCase.execute(dto)
+        expect(result.isLeft()).toBeFalsy()
     })
 })
